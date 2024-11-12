ae40abc582d69f2475f900c14e14442d277173f3
Andreas Kopp <andreas.kopp90@gmail.com>
2024-11-12 15:00:57 +0100

    improved mobile

--- pages/_document.js
+++ pages/_document.js
@@ -27,6 +27,18 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+          />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="Super Activities" />
+          <link rel="apple-touch-icon" href="/logo.png" />
           <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
